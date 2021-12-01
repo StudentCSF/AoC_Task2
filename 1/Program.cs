dd3c969525189248b1c38ddcd0083c59e990f638
Victor Valeev <viktorval2001@gmail.com>
2021-12-01 11:14:38 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();   
+            while(true)
+            {
+                int t = Convert.ToInt32(Console.ReadLine());
+                if (t < 0)
+                {
+                    break;
+                }
+                list.Add(t);
+            }
+
+            int c = 0;
+            for (int i = 3; i < list.Count; i++)
+            {
+                if (list[i] > list[i-3])
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
